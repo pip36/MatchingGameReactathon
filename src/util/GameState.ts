a944ce5a028eb6847061b3e09e0fad45e83c8774
Philip White <philip.white@redington.co.uk>
2023-06-23 12:25:56 +0100

    Add tiles

--- src/util/GameState.ts
+++ src/util/GameState.ts
@@ -21,19 +21,32 @@ type FlipCard = {
 
 type Actions = Action<FlipCard>
 
+const handleFlipCard = (
+  state: GameState,
+  { id }: Action<FlipCard>['payload']
+): GameState => {
+  return state
+}
+
 const reducer = (state: GameState, action: Actions) => {
   switch (action.type) {
     case 'FlipCard':
-      return state
+      return handleFlipCard(state, action.payload)
     default:
       return state
   }
 }
 
+const totalTiles = 16
+
+const initialState: GameState = {
+  tiles: new Array(totalTiles)
+    .fill(0)
+    .map((_, i) => ({ id: i, flipped: false, value: '🐱' })),
+}
+
 export const useGameState = () => {
-  const [state, dispatch] = useReducer(reducer, {
-    tiles: [],
-  })
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const flipCard = (id: number) => dispatch({ type: 'FlipCard', payload: { id } })
 
